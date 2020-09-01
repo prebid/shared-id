85454fc8cca047e6ca7dcf4f20eac048abd48ab2
chammon <chammon@rubiconproject.com>
2020-09-01 15:32:15 -0600

    Removed deploy script

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-eval $(aws ecr get-login --region us-east-1 --no-include-email)
-docker tag shared-id-endpoint:1.1.0 747429369124.dkr.ecr.us-east-1.amazonaws.com/shared-id-endpoint:1.1.0
-docker push 747429369124.dkr.ecr.us-east-1.amazonaws.com/shared-id-endpoint:1.1.0
