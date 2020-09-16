2f3c4f7db41d0d053dc75a65926d0cea6b8cd803
chammon <chammon@rubiconproject.com>
2020-09-16 17:18:13 -0600

    Added explicit number format exception

--- src/main/java/org/sharedid/endpoint/util/IpUtil.java
+++ src/main/java/org/sharedid/endpoint/util/IpUtil.java
@@ -4,31 +4,26 @@ package org.sharedid.endpoint.util;
     Simple util for converting ip addresses from and to integer
     format.
  */
-public class IpUtil
-{
+public class IpUtil {
     //converts ip address in integer format to string format
-    public static String intToIp(int ip)
-    {
+    public static String intToIp(int ip) {
         return ((ip >> 24) & 0xFF) + "." + ((ip >> 16) & 0xFF) + "." + ((ip >> 8) & 0xFF) + "." + (ip & 0xFF);
     }
 
-    public static int ipToInt(String ip) throws IpUtilException
-    {
+    public static int ipToInt(String ip) throws IpUtilException, NumberFormatException {
         String[] addr = ip.split("\\.");
 
-        if (addr.length != 4)
-        {
+        if (addr.length != 4) {
             throw new IpUtilException();
         }
 
         long num = 0;
 
-        for (int i = 0; i < addr.length; i++)
-        {
+        for (int i = 0; i < addr.length; i++) {
             int power = 3 - i;
             num += (Integer.parseInt(addr[i]) % 256 * Math.pow(256, power));
         }
 
-        return (int)num;        //algorithm says return long, but Simon's team expects integer. we should make sure this is ok.
+        return (int) num;
     }
 }
