0565fd107930e471de81a167739b2ab01a34b4f7
chammon <chammon@rubiconproject.com>
2020-10-16 13:23:45 -0600

    Added missing handlers from id routes

--- src/main/java/org/sharedid/endpoint/handler/SetLocationHandler.java
+++ src/main/java/org/sharedid/endpoint/handler/SetLocationHandler.java
@@ -43,7 +43,7 @@ public class SetLocationHandler implements Handler<RoutingContext> {
         DataContext dataContext = DataContext.from(context);
 
         HttpServerResponse response = context.response();
-        
+
         locationService.getCountryForRequest(context.request())
                 .map(geoQuery -> {
                     if (StringUtils.isNotBlank(geoQuery)) {
