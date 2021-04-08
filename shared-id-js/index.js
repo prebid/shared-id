80d3e9ecac6adb5bf89d9af7974ec5e8c016de38
Yaroslav Boychuk <yava.ua@gmail.com>
2021-04-08 11:24:14 +0300

    Update shared-id-js/index.js
    Co-authored-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- shared-id-js/index.js
+++ shared-id-js/index.js
@@ -1,37 +1,14 @@
 import {JSEncrypt} from "jsencrypt";
 
-const URL_SHARED_ID_PUBLIC_KEY = "https://sharedid.org/keys/sharedid-public-key";
+import { JSEncrypt } from "jsencrypt";
 
-const HttpClient = function () {
-    this.get = function (url, cb) {
-        const request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (request.readyState === 4 && request.status === 200)
-                cb(request.responseText);
-        }
-
-        request.open("GET", url, true);
-        request.send(null);
-    }
-}
-const client = new HttpClient();
-
-class SharedID {
-    constructor() {
-        this.encryption = new JSEncrypt();
-        this.initialized = false;
-    }
-
-    set id(value) {
-        if (!this.initialized) {
-            client.get(URL_SHARED_ID_PUBLIC_KEY, response => {
-                this.encryption.setPublicKey(response);
-                document.cookie = `sharedid=${this.encryption.encrypt(value)}`;
-            })
-        } else {
-            document.cookie = `sharedid=${this.encryption.encrypt(value)}`;
-        }
-    }
-}
-
-window.SharedID = SharedID;
+window.sharedIdCookie = function (value, url = "https://id.sharedid.org/keys/sharedid.pub") {
+	fetch(url)
+		.then(response => response.text())
+		.then(key => {
+			const encryption = new JSEncrypt();
+			encryption.setPublicKey(key);
+			document.cookie = `sharedid=${encryption.encrypt(value)}`;
+		})
+		.catch(err => { throw err });
+};
