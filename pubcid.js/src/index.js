3680b23035b53e132209c385eef59627b0f063e8
Yaroslav Boychuk <yboychuk@magnite.com>
2021-07-08 20:25:51 +0300

    collector function registration API in GPT

--- pubcid.js/src/index.js
+++ pubcid.js/src/index.js
@@ -2,13 +2,18 @@ const SOURCE = 'pubcid.org';
 const URL = 'https://id.sharedid.org/pubcid';
 
 window.googletag = window.googletag || {cmd: []};
-window.googletag.encryptedSignalSource = window.googletag.encryptedSignalSource || {};
-googletag.encryptedSignalSource['pubcid.org'] = () => {
-    return new Promise((resolve, reject) =>
-        fetch(URL)
-            .then(response => response.json())
-            .then(data => document.cookie = SOURCE + '=' + data[SOURCE])
-            .then(resolve)
-            .catch(reject)
+window.googletag.encryptedSignalProviders = window.googletag.encryptedSignalProviders || [];
+
+function collector() {
+    return new Promise((resolve, reject) => fetch(URL)
+        .then(response => response.json())
+        .then(data => document.cookie = SOURCE + '=' + data[SOURCE])
+        .then(resolve)
+        .catch(reject)
     );
-};
+}
+
+googletag.encryptedSignalProviders.push({
+    id: 'pubcid.org',
+    collectorFunction: collector
+});
