e79ff69f03b6a04822db274bcbf435af8fd9bed9
Patrick McCann <patmmccann@gmail.com>
2024-10-07 17:06:28 -0400

    Update index.js

--- pubcid.js/src/index.js
+++ pubcid.js/src/index.js
@@ -1,4 +1,4 @@
-console.warn("Warning: The Shared ID Google Deploy Secure Signals script is deprecated in favor of the Prebid User ID method. Please adjust your GAM Secure Signals settings. This script may or may not function as intended. It may have some unusual side effects. See https://support.google.com/admanager/answer/10488752?hl=en&ref_topic=10728657&sjid=15896989004232235718-NA for additional detail.");
+console.warn("Warning: The Shared ID Google Deploy Secure Signals script is deprecated in favor of the Prebid User ID method. Please adjust your GAM Secure Signals settings. This script may or may not function as intended. It may have some unusual side effects. If you are using Prebid.js, the newer method is maintained. If you are not using Prebid, GAM now supports generating its own id analogous to the shared id for AdX requests. See https://support.google.com/admanager/answer/10488752?hl=en&ref_topic=10728657&sjid=15896989004232235718-NA and https://support.google.com/admanager/answer/14082205 for additional detail. Many Prebid publishers have found success using the sharedid as the GAM ppid.");
 
 const SOURCE = 'pubcid.org';
 
